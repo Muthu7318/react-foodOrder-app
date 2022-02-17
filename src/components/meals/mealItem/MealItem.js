61501549d65e637919cadcec41693be5e8143531
Muthu7318 <muthumarshal@gmail.com>
2022-02-17 16:15:06 +0530

    mealItem form

--- src/components/meals/mealItem/MealItem.js
+++ src/components/meals/mealItem/MealItem.js
@@ -1,15 +1,18 @@
 import styles from "./mealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
-    <li className={styles.meals}>
+    <li className={styles.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm></MealItemForm>
+      </div>
     </li>
   );
 };
